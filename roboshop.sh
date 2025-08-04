a39db92e5b10d75b99a6d2a50a92bdbd8c1407cb
K.Manohar <manohar.kunchala12@gmail.com>
2025-08-04 14:47:49 +0530

    aws server creation using automation

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAINE_ID="gayatriraksha.icu"
 for instance in  ${INSTANCES[@]}
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.
-    micro --security-group-ids sg-0f881a0fc38c5ca29 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test}]' --query "Instances[0].
+    micro --security-group-ids sg-0f881a0fc38c5ca29 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].
     InstanceId" --output text)
     if [$instance != "frontend"]
     then
